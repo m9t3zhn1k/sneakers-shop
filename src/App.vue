2e98c12193c2ad339821aa001ad961c3e8aaf1ca
Pavel Korshunau <p.korshunov@section228.com>
2024-05-25 13:02:22 +0300

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
 
-<template></template>
+<template>
+  <div class="bg-white m-auto w-4/5 h-screen rounded-xl shadow-xl mt-8">
+    <Header />
+  </div>
+</template>
 
 <style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 p-8">
+    <div class="flex items-center gap-4">
+      <img class="w-10 h-10" src="/logo.png" alt="Logo" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+    <ul class="flex items-center gap-6">
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 font-normal hover:text-black">
+        <img src="/cart.svg" alt="Logo" /> 1205 руб.
+      </li>
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 font-normal">
+        <img src="/heart.svg" alt="Logo" /> Закладки.
+      </li>
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 font-normal">
+        <img src="/profile.svg" alt="Logo" /> Профиль
+      </li>
+    </ul>
+  </header>
+</template>
