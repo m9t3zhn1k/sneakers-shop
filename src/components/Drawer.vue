f30884a8a64c037572859b9b7b53d6bbf3a3d8b3
Pavel Korshunau <p.korshunov@section228.com>
2024-05-25 22:47:45 +0300

    add drawer with cart

---
+++ src/components/Drawer.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="fixed h-full w-full top-0 left-0">
+    <div class="absolute top-0 left-0 h-full w-full bg-black opacity-30"></div>
+    <slot></slot>
+  </div>
+</template>
